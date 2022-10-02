104797543dd5136bc98caee3db04a372f81d6447
mobilstein <tobiaskim2017@gmail.com>
2022-10-02 16:17:01 +0900

    test2

--- userService/src/main/java/com/example/userservice/jpa/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.userservice.jpa;
-
-import com.example.userservice.jpa.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
-}
